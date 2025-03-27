6fc05bc160647b500f662160f874daa8824cd062
Anirudh Sriram <hello@techwithanirudh.com>
2025-03-27 17:17:56 +0000

    feat: enhance LoginPage layout with HomeLayout and add decorative elements to SignInCard

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -11,10 +11,29 @@ import {
 import { Icons } from '@/components/ui/icons';
 import { signIn } from '@/lib/auth-client';
 import { cn } from '@/lib/utils';
+import { Header } from '@/components/header';
+import { HomeLayout } from 'fumadocs-ui/layouts/home';
+import { getLinks } from 'fumadocs-ui/layouts/shared';
+import { baseOptions, linkItems } from '@/app/layout.config';
+import { PlusIcon } from 'lucide-react';
+
+const Cross = () => (
+  <div className='relative h-6 w-6'>
+    <div className='-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 bg-background'>
+      <PlusIcon size={20} className='text-border/70 dark:text-border' />
+    </div>
+  </div>
+);
 
 function SignInCard() {
   return (
-    <Card className='w-full max-w-xl'>
+    <Card className='relative w-full max-w-xl rounded-none border border-border/70 border-dashed shadow-none dark:border-border'>
+      <div className='-top-3 -left-3 absolute z-10 hidden h-6 sm:block'>
+        <Cross />
+      </div>
+      <div className='-top-3 -right-3 -translate-x-px absolute z-10 hidden h-6 sm:block'>
+        <Cross />
+      </div>
       <CardHeader>
         <CardTitle className='text-lg md:text-xl'>Sign In</CardTitle>
         <CardDescription className='text-xs md:text-sm'>
@@ -31,7 +50,9 @@ function SignInCard() {
           >
             <Button
               variant='outline'
-              className={cn('w-full gap-2')}
+              className={cn(
+                'w-full gap-2 border border-border/70 border-dashed dark:border-border rounded-none',
+              )}
               onClick={async () => {
                 await signIn.social({
                   provider: 'google',
@@ -44,7 +65,9 @@ function SignInCard() {
             </Button>
             <Button
               variant='outline'
-              className={cn('w-full gap-2')}
+              className={cn(
+                'w-full gap-2 border border-border/70 border-dashed dark:border-border rounded-none',
+              )}
               onClick={async () => {
                 await signIn.social({
                   provider: 'github',
@@ -58,14 +81,34 @@ function SignInCard() {
           </div>
         </div>
       </CardContent>
+      <div className='-bottom-3 -left-3 absolute z-10 hidden h-6 sm:block'>
+        <Cross />
+      </div>
+      <div className='-bottom-3 -right-3 -translate-x-px absolute z-10 hidden h-6 sm:block'>
+        <Cross />
+      </div>
     </Card>
   );
 }
 
 export default function LoginPage() {
   return (
-    <main className='flex min-h-dvh flex-1 items-center justify-center px-4 sm:px-8 md:px-12 lg:px-16 2xl:px-24'>
-      <SignInCard />
-    </main>
+    <HomeLayout
+      {...baseOptions}
+      links={linkItems}
+      nav={{
+        component: (
+          <Header
+            finalLinks={getLinks(linkItems, baseOptions.githubUrl)}
+            {...baseOptions}
+          />
+        ),
+      }}
+      className='border-grid pt-0'
+    >
+      <main className='flex flex-1 items-center justify-center px-4 sm:px-8 md:px-12 lg:px-16 2xl:px-24 container-wrapper'>
+        <SignInCard />
+      </main>
+    </HomeLayout>
   );
 }
