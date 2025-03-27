36bb2b62f0a72f9db602e2410b904833a2b3c812
Anirudh Sriram <hello@techwithanirudh.com>
2025-03-27 15:11:12 +0000

    feat: enhance newsletter form layout and improve user feedback

--- src/app/(home)/_components/call-to-action.tsx
+++ src/app/(home)/_components/call-to-action.tsx
@@ -19,7 +19,7 @@ export function CTA(): React.ReactElement {
             </p>
           </div>
 
-          <div className=''>
+          <div className='flex items-center w-full'>
             <NewsletterForm />
           </div>
         </div>
--- src/components/newsletter-form.tsx
+++ src/components/newsletter-form.tsx
@@ -5,85 +5,97 @@ import { useAction } from 'next-safe-action/hooks';
 import type { Newsletter } from '@/lib/validators';
 import { Button } from '@/components/ui/button';
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
 } from '@/components/ui/form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { Input } from '@/components/ui/input';
 import { NewsletterSchema } from '@/lib/validators';
 
-import { TriangleAlertIcon as IconWarning, CheckCircleIcon as IconCheckCircle } from 'lucide-react';
+import {
+    CircleCheckIcon,
+  SendIcon,
+  TriangleAlertIcon,
+} from 'lucide-react';
 import { Alert, AlertTitle } from '@/components/ui/alert';
 
 import { subscribeUser } from '@/app/(home)/actions';
 import { LoaderIcon } from 'lucide-react';
 
 export const NewsletterForm = () => {
-    const form = useForm({
-        resolver: zodResolver(NewsletterSchema),
-        defaultValues: {
-            email: '',
-        },
-    });
+  const form = useForm({
+    resolver: zodResolver(NewsletterSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
 
-    const { execute, result, status } = useAction(subscribeUser);
+  const { execute, result, status } = useAction(subscribeUser);
 
-    const onSubmit = (values: Newsletter) => {
-        execute(values);
-    };
+  const onSubmit = (values: Newsletter) => {
+    execute(values);
+  };
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                <div className="space-y-2">
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormControl>
-                                    <Input
-                                        {...field}
-                                        disabled={status === 'executing'}
-                                        placeholder="Email address"
-                                        type="email"
-                                        className='border-border'
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4 flex-1'>
+        <div className='overflow-hidden rounded-md border bg-muted p-0 flex min-h-10 h-full'>
+          <div className='flex-1'>
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem className='h-full group'>
+                  <FormControl className='h-full group-has-[p]:pt-3'>
+                    <Input
+                      {...field}
+                      disabled={status === 'executing'}
+                      placeholder='Email address'
+                      type='email'
+                      className='border-none rounded-md rounded-r-none px-4 focus-visible:ring-0 focus-visible:ring-offset-0 h-full'
                     />
-                </div>
+                  </FormControl>
+                  <FormMessage className='ml-4 text-xs pb-2' />
+                </FormItem>
+              )}
+            />
+          </div>
 
-                {status === 'hasSucceeded' && (
-                    <Alert className="bg-emerald-500/15 text-emerald-500 p-3 border-emerald-500/15">
-                        <IconCheckCircle size={16} />
-                        <AlertTitle className='mb-0 leading-normal'>You are now subscribed to our newsletter!</AlertTitle>
-                    </Alert>
-                )}
-                {result.serverError && (
-                    <Alert className="bg-destructive/15 text-destructive dark:bg-destructive dark:text-destructive-foreground p-3 border-destructive/15 dark:border-destructive">
-                        <IconWarning className='size-4' />
-                        <AlertTitle className='mb-0 leading-normal'>{result.serverError}</AlertTitle>
-                    </Alert>
-                )}
+          <Button
+            disabled={status === 'executing'}
+            type='submit'
+            size='icon'
+            className='rounded-md rounded-l-none px-3 size-auto'
+          >
+            {status === 'executing' ? (
+              <LoaderIcon className='size-4 animate-spin mr-1' />
+            ) : (
+              <SendIcon className='size-4 mr-1' />
+            )}
+          </Button>
+        </div>
 
-                <Button
-                    disabled={status === 'executing'}
-                    type="submit"
-                    className="w-full"
-                >
-                    {status === 'executing' && (
-                        <LoaderIcon className="mr-2 size-4 animate-spin" />
-                    )}
-                    Continue with Email
-                </Button>
-            </form>
-        </Form>
-    );
+        {status === 'hasSucceeded' && (
+          <Alert className='bg-emerald-500/15 text-emerald-500 p-3 border-emerald-500/15 rounded-full'>
+            <CircleCheckIcon size={16} />
+            <AlertTitle className='mb-0 leading-normal'>
+              You are now subscribed to our newsletter!
+            </AlertTitle>
+          </Alert>
+        )}
+        {result.serverError && (
+          <Alert className='bg-destructive/15 text-destructive dark:bg-destructive dark:text-destructive-foreground p-3 border-destructive/15 dark:border-destructive rounded-full'>
+            <TriangleAlertIcon className='size-4' />
+            <AlertTitle className='mb-0 leading-normal'>
+              {result.serverError}
+            </AlertTitle>
+          </Alert>
+        )}
+      </form>
+    </Form>
+  );
 };
