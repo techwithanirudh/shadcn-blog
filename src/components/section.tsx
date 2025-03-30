1d949e7bd765a484a4950afa0dd9ec64818c9624
Anirudh Sriram <hello@techwithanirudh.com>
2025-03-30 13:13:06 +0000

    refactor: streamline CTA component layout and remove unused ViewAnimation provider

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,5 +1,4 @@
 import { cn } from '@/lib/utils';
-import { ViewAnimation } from '@/providers/view-animation';
 import { PlusIcon } from 'lucide-react';
 import type { HTMLAttributes } from 'react';
 
@@ -10,13 +9,11 @@ const Cross = () => (
     <div className="absolute left-3 h-6 w-px bg-background" />
     <div className="absolute top-3 h-px w-6 bg-background" />
 
-    <ViewAnimation
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
+    <div
       className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2"
     >
       <PlusIcon size={20} className="text-border" />
-    </ViewAnimation>
+    </div>
   </div>
 );
 
